01eec104fa24f74b07cc282fb2c0e751dc69549f
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-04-23 21:07:43 +0900

    mysql user 추가 된거 확인

--- srcs/requirements/mariadb/scripts/init-db.sh
+++ srcs/requirements/mariadb/scripts/init-db.sh
@@ -17,11 +17,11 @@ echo "MariaDB is ready."
 
 # 데이터베이스를 생성합니다.
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS mydb;"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 
 # 사용자를 생성하고 권한을 부여합니다.
-mysql -u root -e "CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'mypassword';"
-mysql -u root -e "GRANT ALL ON mydb.* TO 'myuser'@'%';"
+mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -u root -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
 
 # MariaDB 서비스를 안전하게 종료합니다.
 rc-service mariadb stop
