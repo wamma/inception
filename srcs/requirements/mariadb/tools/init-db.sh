96a1095e5d54d20f8639a1f8c9d6fa6cbfc46235
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-04-24 18:44:41 +0900

    mariadb 설정 파일들 수정

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -1,29 +1,38 @@
 #!/bin/sh
 
-# MariaDB 서비스를 시작합니다.
-
+# OpenRC 서비스 초기화
 openrc default
 
+# MariaDB 초기 설정
 /etc/init.d/mariadb setup
 
+# MariaDB 서비스 시작
 rc-service mariadb start
 
-# MariaDB 서버가 준비될 때까지 기다린다
-# while ! mysqladmin ping --silent; do
-# 	sleep 1
-# done
+# MariaDB 서버가 준비될 때까지 기다립니다
+while ! mysqladmin ping --silent; do
+    sleep 1
+done
 
 echo "MariaDB is ready."
 
-# 데이터베이스를 생성합니다.
+# 데이터베이스가 이미 존재하는지 확인
+RESULT=`mysql -u root -e "SHOW DATABASES LIKE '$MYSQL_DATABASE';"`
+if [ "$RESULT" == "" ]; then
+    # 데이터베이스 생성
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+    # 사용자 생성 및 권한 부여
+    mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+else
+    echo "Database and user already exist."
+fi
 
-# 사용자를 생성하고 권한을 부여합니다.
-mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -u root -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+echo "MariaDB create success."
 
 # MariaDB 서비스를 안전하게 종료합니다.
 rc-service mariadb stop
 
-mysqld_safe
+# MariaDB 안전 실행
+exec /usr/bin/mysqld_safe
