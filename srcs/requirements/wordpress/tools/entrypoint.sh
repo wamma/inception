db2973013c198fabed380c50e258841d74f5cac4
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-04-29 17:06:55 +0900

    오류는 나지만 docker-compose는 된다. Makefile 파일 생성

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# MariaDB 서버가 준비될 때까지 기다립니다
-/wait-for-it.sh mariadb:3306 --timeout=60
+# MariaDB 서버가 준비될 때까지 기다립니다(경로에 없음)
+# /wait-for-it.sh mariadb:3306 --timeout=60
 
 # wp-config.sh를 실행하여 WordPress 설정을 수행
-/usr/bin/wp-config
+/usr/local/bin/wp-config
 
 # PHP-FPM을 시작합니다
 php-fpm8 -F
